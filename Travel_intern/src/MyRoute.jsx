0cbec285dce7e1afbb4acdff81c290aff55343bc
Samrat Tamrakar <samrattamrakar7@gmail.com>
2025-03-19 19:22:16 +0545

    minor error fix

--- Travel_intern/src/MyRoute.jsx
+++ Travel_intern/src/MyRoute.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
-import { Route } from 'react-router'
-import { Router } from 'react-router'
+import { Route, Routes } from 'react-router'
 import { BrowserRouter } from 'react-router'
 import Login from './pages/Login'
 import Layout from './layout/Layout'
 
 const MyRoute = () => {
   return (
+    
     <BrowserRouter>
-        <Router>
-            <Route path='/' element={<Layout />}>
+      <Routes>
+        <Route path='/' element={<Layout />}>
           <Route path='/login' element={<Login />} />
+         
+        </Route>
 
-
-            </Route>
-        </Router>
+      </Routes>
     </BrowserRouter>
-
 )
 }
 
