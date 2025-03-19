95d77636a36d94eca6f272f0f5abb48bd0e84394
Rajeshan Maharjan <rajeshanmaharjan.25@gmail.com>
2025-03-19 17:21:33 +0545

    edit file

--- Travel_intern/src/App.jsx
+++ Travel_intern/src/App.jsx
@@ -8,6 +8,7 @@ const App = () => {
     <div className="bg-yellow-400 font-extrabold text-8xl">test</div>
     <h1>Hwlooer yest</h1>
     <div className="bg-orange-500">Orange</div>
+    <div>Helo</div>
     
     </>
   )
