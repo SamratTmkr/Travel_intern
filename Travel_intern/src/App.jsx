47a191f3773831f4597b60444397d42bd3970bdc
Rajeshan Maharjan <rajeshanmaharjan.25@gmail.com>
2025-03-21 21:14:38 +0545

    layout changing and put on header and footer

--- Travel_intern/src/App.jsx
+++ Travel_intern/src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import Layout from './layout/Layout'
 import { Outlet } from 'react-router'
+import Header from './layout/Header'
+import Footer from './layout/Footer'
 
 const App = () => {
   return (
     <>
 
-  <Layout/>
+<Header/>
    <main className='w-full h-full'>
    <Outlet/>
    </main>
-      <main>
-        <Layout />
-      </main>
+<Footer/>
 
     </>
   )
